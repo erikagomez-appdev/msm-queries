24fb08da727af1bc1c119aa16ee17f219acb03a8
erikag213 <erika.gomez@chicagobooth.edu>
2021-04-28 21:22:42 +0000

    id progress

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,4 +5,26 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/index.html.erb" })
   end
 
+  def  eldest  
+
+    @oldest = Director.where.not({ :dob => nil }).order({:dob => :asc}).at(0)
+    render({ :template => "director_templates/eldest.html.erb" })
+  end
+
+  def  youngest  
+
+    @youngest = Director.where.not({ :dob => nil }).order({:dob => :desc}).at(0)
+    render({ :template => "director_templates/youngest.html.erb" })
+  end
+
+  def  director_details  
+
+    #Parameters: {"id"=>"1"}
+    the_id = params.fetch("id")
+
+    @the_director = Director.where({ :id => the_id })
+
+    render({ :template => "director_templates/anid.html.erb" })
+  end
+
 end
